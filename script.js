3b9567a0b48b68e64a73ccf9c4acb8fff3661685
Camden Miller <camdenquin@gmail.com>
2020-06-08 17:54:21 -0400

    all functionality implemented

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 const STORE = {
   tipTotal: 0,
   mealCount: 0
-}
+};
 
 function generateCustCharges(subtotal,tip,total) {
   console.log('generateCustCharges ran!');
   return `<h2>Customer Charges</h2>
-          <p>Subtotal: ${subtotal}</p>
-          <p>Tip: ${tip}</p>
-          <br>
-          <p>Total: ${total}</p>`;
+          <p>Subtotal: $${subtotal}</p>
+          <p>Tip: $${tip}</p>
+          <hr class="line"> 
+          <p>Total: $${total}</p>`;
 }
 
 function generateEarningsInfo(tipPerMeal) {
   console.log('generateEarningsInfo ran!');
   return `<h2>My Earnings Info</h2>
-          <p>Tip Total: ${STORE.tipTotal}</p>
+          <p>Tip Total: $${STORE.tipTotal}</p>
           <p>Meal Count: ${STORE.mealCount}</p>
-          <p>Average Tip Per Meal: ${tipPerMeal}</p>
-          <button type="button" id="resetEverything">Reset</button>`;
+          <p>Average Tip Per Meal: $${tipPerMeal}</p>
+          <form id="resetForm">
+            <fieldset>
+              <button type="submit" id="resetEverything">Reset</button>
+            </fieldset>
+          </form>`;
 }
 
 function calculateCharges(baseMealPrice,taxRate,tipPercent) {
   console.log('calculateCharges ran!');
-  const subtotal = baseMealPrice * taxRate + baseMealPrice,
-    tip = tipPercent * baseMealPrice,
-    total = subtotal + tip;
-    STORE.tipTotal += tip;
+  let subtotal = baseMealPrice * taxRate + baseMealPrice,
+    tip = tipPercent * baseMealPrice;
+
+  subtotal = Number(subtotal.toFixed(2));
+  tip = Number(tip.toFixed(2));
+
+  const total = subtotal + tip;
+
+  STORE.tipTotal += tip;
+
   return [subtotal, tip, total];
 }
 
@@ -58,15 +68,17 @@ function handleSubmitMealDetails() {
     renderEarningsInfo();
   });
 }
+function resetEverything() {
+  STORE.tipTotal = 0;
+  STORE.mealCount = 0;
+  console.log(STORE.tipTotal);
+  console.log(STORE.mealCount);
+  renderCustCharges(0,0,0);
+  renderEarningsInfo();
+}
 
 function handleResetEverything() {
-  $('#resetEverything').on('click', event => {
-    event.preventDefault();
-    STORE.tipTotal = 0;
-    STORE.mealCount = 0;
-    renderCustCharges(0,0,0);
-    renderEarningsInfo();
-  });
+  $('#resetForm').on('submit', resetEverything());
 }
 
 function appHandlers() {

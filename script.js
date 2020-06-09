53bdea78e95bcd90a3dfc752015b8291bc4bc4b6
Camden Miller <camdenquin@gmail.com>
2020-06-09 13:22:49 -0400

    removed console.log statements

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const STORE = {
 };
 
 function generateCustCharges(subtotal,tip,total) {
-  console.log('generateCustCharges ran!');
   return `<h2>Customer Charges</h2>
           <p>Subtotal: $${subtotal}</p>
           <p>Tip: $${tip}</p>
@@ -13,7 +12,6 @@ function generateCustCharges(subtotal,tip,total) {
 }
 
 function generateEarningsInfo(tipPerMeal) {
-  console.log('generateEarningsInfo ran!');
   return `<h2>My Earnings Info</h2>
           <p>Tip Total: $${Number(STORE.tipTotal.toFixed(2))}</p>
           <p>Meal Count: ${Number(STORE.mealCount.toFixed(2))}</p>
@@ -26,7 +24,6 @@ function generateEarningsInfo(tipPerMeal) {
 }
 
 function calculateCharges(baseMealPrice,taxRate,tipPercent) {
-  console.log('calculateCharges ran!');
   let subtotal = baseMealPrice * taxRate + baseMealPrice,
     tip = tipPercent * baseMealPrice;
 
@@ -41,7 +38,6 @@ function calculateCharges(baseMealPrice,taxRate,tipPercent) {
 }
 
 function renderCustCharges(baseMealPrice,taxRate,tipPercent) {
-  console.log('renderCustCharges ran!');
   const calculatedCarges = calculateCharges(baseMealPrice,taxRate,tipPercent),
     html = generateCustCharges(...calculatedCarges);
   $('#custCharges').html(html);
@@ -59,16 +55,13 @@ function calculateEarningsInfo() {
 }
 
 function renderEarningsInfo() {
-  console.log('renderEarningsInfo ran!');
   const calculatedEarnings = calculateEarningsInfo(),
     html = generateEarningsInfo(calculatedEarnings);
   $('#earningsInfo').html(html);
 }
 
 function handleSubmitMealDetails() {
-  console.log('handleSubmitMealDetails ran!');
   $('#mealDetails').on('submit', event => {
-    console.log('submit handler ran!');
     event.preventDefault();
     const baseMealPrice = Number($('#basePrice').val()),
       taxRate = Number($('#taxRate').val())/100,
@@ -81,8 +74,6 @@ function handleSubmitMealDetails() {
 function resetEverything() {
   STORE.tipTotal = 0;
   STORE.mealCount = 0;
-  console.log(STORE.tipTotal);
-  console.log(STORE.mealCount);
   renderCustCharges(0,0,0);
   renderEarningsInfo();
 }
